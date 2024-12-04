ff66fbbfd37d2ff26987f7eeacfaaaf925a22533
tgibbons-css <tgibbons@css.edu>
2024-12-04 14:02:41 -0600

    Try 2 for GitHub

---
+++ src/main/MainProg.java
@@ -0,0 +1,15 @@
+package main;
+
+public class MainProg {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello from Main 2");
+		
+		
+		// test
+		
+		System.out.println("Push to GITHUB?");
+	}
+
+}
